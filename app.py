53165932bfc5b753459ee3b956962c1c14b5823e
Sapan Gajjar <sapangajjar101105@gmail.com>
2023-12-16 15:45:16 +0530

    newpush

--- app.py
+++ app.py
@@ -7,16 +7,26 @@ cachedBody = []
 
 json_data = response.json()
 
-
-
-sl.title("VidSrc: Display")
-
-sl.write("> Latest Released Episodes Here!")
-
 for i in range(len(json_data["result"])):
     cachedBody.append(json_data["result"][i])
-    sl.write(f"""
-| Method | Accuracy |
-| ----------- | ----------- |
-| {json_data["result"][i][1]} | {json_data["result"][i][0]}% |
+
+markdown_code = ""
+
+# Iterate through the list
+for item in cachedBody:
+    markdown_code += f"""
+| {item["show_title"]} | {item["season"]} | {item["episode"]} | [Play]({item["embed_url"]}) |
+|----------------------|------------------|-------------------|-----------------------------|
+"""
+
+sl.title("VidSrc: Display")
+sl.write("> Latest Released Episodes Here!")
+sl.write(f"""
+| Name | Season | Episode | URL|
+|------|--------|---------|----|
+{markdown_code}
 """)
+
+# with sl.expander("Provide values for input features"):
+#     for i in range(len(json_data["result"])):
+#         print({json_data["result"][i][1]})
